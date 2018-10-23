d56d09b36449b34b337b710a8f9313c1047e0ea3
Kai Wang <kwang57@jhu.edu>
2018-10-23 00:21:08 -0400

    Attention function

--- seq2seq.py
+++ seq2seq.py
@@ -135,6 +135,15 @@ def tensors_from_pair(src_vocab, tgt_vocab, pair):
 ######################################################################
 
 
+# class LSTM(nn.module):
+#
+#     def __init__(self, input_size, hidden_size):
+#         super(LSTM, self).__init__()
+#         self.hidden_size = hidden_size
+#
+
+
+
 class EncoderRNN(nn.Module):
     """the class for the enoder RNN
     """
@@ -393,7 +402,22 @@ def show_attention(input_sentence, output_words, attentions):
     """
     
     "*** YOUR CODE HERE ***"
-    raise NotImplementedError
+    # Set up figure with colorbar
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(attentions.numpy(), cmap='bone')
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticklabels([''] + input_sentence.split(' ') +
+                       ['<EOS>'], rotation=90)
+    ax.set_yticklabels([''] + output_words)
+
+    # Show label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
 
 
 def translate_and_show_attention(input_sentence, encoder1, decoder1, src_vocab, tgt_vocab):
